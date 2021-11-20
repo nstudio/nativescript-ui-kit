f5023148173cec147c291397e807ec08aaf83cde
Nathan Walker <walkerrunpdx@gmail.com>
2021-11-20 09:49:41 -0800

    fix(ios): edge case with padding from baseline Label class
    closes https://github.com/nstudio/nativescript-ui-kit/issues/1

--- packages/nativescript-label-marquee/index.ios.ts
+++ packages/nativescript-label-marquee/index.ios.ts
@@ -1,30 +1,31 @@
 import { fadeLengthProperty, labelizeProperty, LabelMarqueeCommon, scrollDurationProperty } from './common';
 
 export class LabelMarquee extends LabelMarqueeCommon {
+  // @ts-ignore
+  get ios(): MarqueeLabel {
+    return this.nativeView;
+  }
 
-    // @ts-ignore
-    get ios(): MarqueeLabel {
-        return this.nativeView;
-    }
-    
-    createNativeView() {
-        return MarqueeLabel.alloc().init();
-    }
+  createNativeView() {
+    return MarqueeLabel.alloc().init();
+  }
 
-    initNativeView() {
-        this.ios.fadeLength = 10;
-        this.ios.scrollDuration = 8;
-    }
+  initNativeView() {
+    this.ios.fadeLength = 10;
+    this.ios.scrollDuration = 8;
+    // fix edge case - will be properly fixed in @nativescript/core 8.2
+    this.nativeView.padding = { top: 0, right: 0, bottom: 0, left: 0 };
+  }
 
-    [fadeLengthProperty.setNative](value: number) {
-		this.ios.fadeLength = value;
-	}
+  [fadeLengthProperty.setNative](value: number) {
+    this.ios.fadeLength = value;
+  }
 
-    [scrollDurationProperty.setNative](value: number) {
-		this.ios.scrollDuration = value;
-	}
+  [scrollDurationProperty.setNative](value: number) {
+    this.ios.scrollDuration = value;
+  }
 
-    [labelizeProperty.setNative](value: boolean) {
-		this.ios.labelize = value;
-	}
+  [labelizeProperty.setNative](value: boolean) {
+    this.ios.labelize = value;
+  }
 }
