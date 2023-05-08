66739aab7597b805b31436a9568914bb4ee45cf8
Nathan Walker <walkerrunpdx@gmail.com>
2023-05-08 14:50:53 -0700

    fix(fonticon): allow flavor debug to affect factory

--- packages/nativescript-fonticon/angular/fonticon.service.ts
+++ packages/nativescript-fonticon/angular/fonticon.service.ts
@@ -8,9 +8,11 @@ export const USE_STORE = new InjectionToken<any>("USE_STORE");
   providedIn: 'root'
 })
 export class FontIconService {
-
+  static debug = false;
+  
   constructor(@Inject(USE_STORE) private config: any) {
     FontIconFactory.paths = this.config;
+    FontIconFactory.debug = FontIconService.debug;
     FontIconFactory.loadCss();
   }
 }
