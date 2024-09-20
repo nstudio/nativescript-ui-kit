b643e2fa40381f0cf7a6e4ee4797c73cb42679e1
Osei Fortune <fortune.osei@yahoo.com>
2024-09-20 01:23:39 -0400

    feat(coachmarks): more customizations for android (#28)

--- tools/demo/nativescript-coachmarks/index.ts
+++ tools/demo/nativescript-coachmarks/index.ts
@@ -1,6 +1,6 @@
 import { DemoSharedBase } from '../utils';
 import { ICoachMarkOptions, CoachMark, CoachMarks } from '@nstudio/nativescript-coachmarks';
-import { Color } from '@nativescript/core';
+import { Color, Screen } from '@nativescript/core';
 
 export class DemoSharedNativescriptCoachmarks extends DemoSharedBase {
   private _coachMarks;
@@ -17,8 +17,11 @@ export class DemoSharedNativescriptCoachmarks extends DemoSharedBase {
   }
 
   loadedMenuItem(args) {
-    this._menu = args.object.ios;
-    
+    this._menu = args.object.nativeView;
+
+    if (__ANDROID__) {
+      this._menu = args.object.getViewById('menu')?.nativeView;
+    }
 
     CoachMarks.DEBUG = true; // view logs of coach marks setup
   }
@@ -33,16 +36,18 @@ export class DemoSharedNativescriptCoachmarks extends DemoSharedBase {
 
     let options: ICoachMarkOptions = {
       continueLabelText: 'Tap Screen for Next Tip',
+      continueLabelBackgroundColor: '#FFE108',
+      continueLabelOffset: { y: -60 },
       skipButtonText: 'Exit',
+      skipButtonOffset: { y: -60 },
       lblSpacing: 20,
-      maskColor: global.isIOS ? UIColor.colorWithRedGreenBlueAlpha(0.30, 0.46, 0.89, .9) : null
+      lblTextColor: '#FFE108',
+      maskColor: global.isIOS ? UIColor.colorWithRedGreenBlueAlpha(0.3, 0.46, 0.89, 0.9) : new Color(0.9 * 255, 0.3 * 255, 0.46 * 255, 0.89 * 255),
     };
-    CoachMarks.start(this.getMarks(), options, this._coachMarks);    
+    CoachMarks.start(this.getMarks(), options, this._coachMarks);
   }
 
   private getMarks() {
-   
-
     if (global.isIOS) {
       let menuPosition = this._menu.frame;
       let firstPosition = this._first.ios.frame;
@@ -50,71 +55,83 @@ export class DemoSharedNativescriptCoachmarks extends DemoSharedBase {
       let thirdPosition = this._third.ios.frame;
       return [
         new CoachMark({
-          position: CGRectMake(
-            menuPosition.origin.x +164, 
-            menuPosition.origin.y + 58, 
-            menuPosition.size.width + 10, 
-            menuPosition.size.height
-          ),
+          position: CGRectMake(menuPosition.origin.x + 164, menuPosition.origin.y + 58, menuPosition.size.width + 10, menuPosition.size.height),
           caption: '1. Main menu.',
           shape: CoachMark.SHAPES.DEFAULT,
           labelPosition: CoachMark.LABEL_POSITIONS.RIGHT_BOTTOM,
           labelAlignment: CoachMark.LABEL_ALIGNMENTS.CENTER,
-          showArrow: true
+          showArrow: true,
         }),
         new CoachMark({
-          position: CGRectMake(
-            firstPosition.origin.x, 
-            firstPosition.origin.y, 
-            firstPosition.size.width, 
-            firstPosition.size.height
-          ),
+          position: CGRectMake(firstPosition.origin.x, firstPosition.origin.y, firstPosition.size.width, firstPosition.size.height),
           caption: '2. View customer listings.',
           shape: CoachMark.SHAPES.DEFAULT,
           labelPosition: CoachMark.LABEL_POSITIONS.BOTTOM,
           labelAlignment: CoachMark.LABEL_ALIGNMENTS.CENTER,
-          showArrow: true
+          showArrow: true,
         }),
         new CoachMark({
-          position: CGRectMake(
-            thirdPosition.origin.x, 
-            thirdPosition.origin.y, 
-            thirdPosition.size.width, 
-            thirdPosition.size.height
-          ),
+          position: CGRectMake(thirdPosition.origin.x, thirdPosition.origin.y, thirdPosition.size.width, thirdPosition.size.height),
           caption: '3. View account settings.',
           shape: CoachMark.SHAPES.DEFAULT,
           labelPosition: CoachMark.LABEL_POSITIONS.TOP,
           labelAlignment: CoachMark.LABEL_ALIGNMENTS.CENTER,
-          showArrow: true
+          showArrow: true,
         }),
         new CoachMark({
-          position: CGRectMake(
-            secondPosition.origin.x - 5, 
-            secondPosition.origin.y - 5, 
-            secondPosition.size.width + 10, 
-            secondPosition.size.height + 10
-          ),
+          position: CGRectMake(secondPosition.origin.x - 5, secondPosition.origin.y - 5, secondPosition.size.width + 10, secondPosition.size.height + 10),
           caption: '4. Just a circular shape.',
           shape: CoachMark.SHAPES.CIRCLE,
           labelPosition: CoachMark.LABEL_POSITIONS.BOTTOM,
-          labelAlignment: CoachMark.LABEL_ALIGNMENTS.CENTER
-        })
+          labelAlignment: CoachMark.LABEL_ALIGNMENTS.CENTER,
+        }),
       ];
     } else {
+      const sizeOffset = 10 * Screen.mainScreen.scale;
       return [
         new CoachMark({
-          view: this._first,
-          caption: '1. View customer listings.',
+          position: {
+            x: 0,
+            y: 0,
+            width: this._menu.getMeasuredWidth() + sizeOffset,
+            height: this._menu.getMeasuredHeight(),
+          },
+          view: this._menu,
+          shape: CoachMark.SHAPES.DEFAULT,
+          caption: '1. Main menu.',
+          labelPosition: CoachMark.LABEL_POSITIONS.RIGHT_BOTTOM,
+          labelAlignment: CoachMark.LABEL_ALIGNMENTS.CENTER,
+          // showArrow: true,
         }),
         new CoachMark({
-          view: this._second,
-          caption: '2. View account settings.',
+          view: this._first,
+          shape: CoachMark.SHAPES.DEFAULT,
+          caption: '2. View customer listings.',
+          labelPosition: CoachMark.LABEL_POSITIONS.BOTTOM,
+          labelAlignment: CoachMark.LABEL_ALIGNMENTS.CENTER,
+          // showArrow: true,
         }),
         new CoachMark({
           view: this._third,
-          caption: '3. Just a circular shape.',
-        })
+          shape: CoachMark.SHAPES.DEFAULT,
+          caption: '3. View account settings.',
+          labelPosition: CoachMark.LABEL_POSITIONS.TOP,
+          labelAlignment: CoachMark.LABEL_ALIGNMENTS.CENTER,
+          // showArrow: true,
+        }),
+        new CoachMark({
+          position: {
+            x: 0,
+            y: 0,
+            width: this._second.getMeasuredWidth() + sizeOffset,
+            height: this._second.getMeasuredHeight() + sizeOffset,
+          },
+          view: this._second,
+          shape: CoachMark.SHAPES.CIRCLE,
+          caption: '4. Just a circular shape.',
+          labelPosition: CoachMark.LABEL_POSITIONS.BOTTOM,
+          labelAlignment: CoachMark.LABEL_ALIGNMENTS.CENTER,
+        }),
       ];
     }
   }
@@ -139,7 +156,7 @@ export class DemoSharedNativescriptCoachmarks extends DemoSharedBase {
   private customizeStyle(data: any) {
     // Note: just experimentation for settings
     // console.log(data.instance.arrowImage.image);
-    // could customize the arrowImage at each step 
+    // could customize the arrowImage at each step
     // data.instance.arrowImage.image = UIImage.imageNamed('someimage.png');
     // console.log(data.instance.btnSkipCoach);
     // console.log(data.instance.lblCaption);
@@ -150,14 +167,14 @@ export class DemoSharedNativescriptCoachmarks extends DemoSharedBase {
       // it disappears after the first tap and advance to next step
       let labelContinue = data.instance.lblContinue.frame;
       data.instance.lblContinue.frame = CGRectMake(labelContinue.origin.x, labelContinue.origin.y - 60, labelContinue.size.width, labelContinue.size.height + 60);
-      data.instance.lblContinue.backgroundColor = new Color('#FFE108').ios; 
-      
+      data.instance.lblContinue.backgroundColor = new Color('#FFE108').ios;
+
       // custom caption color
       data.instance.lblCaption.textColor = new Color('#FFE108').ios;
 
       // customize skip button
       let btnSkip = data.instance.btnSkipCoach.frame;
       data.instance.btnSkipCoach.frame = CGRectMake(btnSkip.origin.x, btnSkip.origin.y - 60, btnSkip.size.width, btnSkip.size.height + 60);
-    }    
+    }
   }
 }
