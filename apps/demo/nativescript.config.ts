a967b77f48ceb27a564f014be85edfef6781257a
Nathan Walker <walkerrunpdx@gmail.com>
2026-02-17 19:04:44 -0800

    chore: workspace updates

--- apps/demo/nativescript.config.ts
+++ apps/demo/nativescript.config.ts
@@ -1,24 +1,14 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-	id: 'org.nativescript.plugindemo',
-	appResourcesPath: '../../tools/assets/App_Resources',
-	android: {
-		v8Flags: '--expose_gc',
-		markingMode: 'none',
-	},
-	appPath: 'src',
-	cli: {
-		packageManager: 'npm'
-	},
-	ios: {
-		SPMPackages: [
-			{
-				 name: "SmartlookAnalytics",
-				 libs: ["SmartlookAnalytics"],
-				 repositoryURL: "https://github.com/smartlook/analytics-swift-package.git",
-				 version: "2.2.9"
-			}
-		]
-	}
+  id: 'org.nativescript.plugindemo',
+  appResourcesPath: '../../tools/assets/App_Resources',
+  android: {
+    v8Flags: '--expose_gc',
+    markingMode: 'none',
+  },
+  appPath: 'src',
+  cli: {
+    packageManager: 'npm',
+  },
 } as NativeScriptConfig;
